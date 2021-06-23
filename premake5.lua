24e6a34b14d7fdb771a4433004af2c93485bc28f
Ank-G <79757544+Ank-G@users.noreply.github.com>
2021-06-23 13:46:57 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -20,9 +20,9 @@ project "GLFW"
 	}
 
 	filter "system:windows"
-    buildoptions { "-std=c11", "-lgdi32" }
+		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
-    staticruntime "On"
+    		staticruntime "On"
 
 		files
 		{
